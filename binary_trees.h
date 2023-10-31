1a54199a84fb055602bd6abb38275b04d187e1ff
KelvinoKing <kelvinoking@outlook.com>
2023-10-31 09:16:32 +0000

    Header file

--- binary_trees.h
+++ binary_trees.h
@@ -24,4 +24,6 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+void binary_tree_print(const binary_tree_t);
+
 #endif /*BINARY_TREES_H*/
