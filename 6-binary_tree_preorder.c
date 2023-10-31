e74bf4aa3c6cbc80a98e59107be9cf4092fcb57d
KelvinoKing <kelvinoking@outlook.com>
2023-10-31 12:18:13 +0000

    preorder traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Goes through binary tree using pre-order traversal
+ *
+ * @tree: pointer to the rood node of tree to traverse
+ * @func: pointer to function to call for each node
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
